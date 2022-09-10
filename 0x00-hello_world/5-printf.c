fb6875a227dc2da637061b60ae2d17938acd817c
anushiferaw <anuhabyba@gmail.com>
2022-09-10 01:48:28 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stidio.h>
+#include <stdio.h>
 /**
-*main - Entry point
-*
+*main - Entry point*
 * Return: Always 0 (Success)
-* */ 
+*/ 
 int main(void) 
 {
- printf("with proper grammer, butthe out come is a piece art,\n");
- return (0);
+printf("with proper grammer, butthe out come is a piece art,\n");
+return (0);
 }
