ce52c80d3c078c6efa029bceee4290e33b572c8f
anushiferaw <anuhabyba@gmail.com>
2022-09-10 07:52:28 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - entry point, prints astringe to stderr
+ *
+ * Return: returns 1(fail)
+ */
+int main(void)
+{
+   write (2, "and that piece of art is useful\"- Dora  Korpar, 2015-10-19\n", 59);
+   return (1);
+}
