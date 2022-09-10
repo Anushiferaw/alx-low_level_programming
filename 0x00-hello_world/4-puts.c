366d73a707c34edf2c9a0c89f3f725faba3414bc
anushiferaw <anuhabyba@gmail.com>
2022-09-09 22:01:00 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
- * Return: Alwys 0 (success)
- */
+* Return: Alwys 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+return (0);
+}
