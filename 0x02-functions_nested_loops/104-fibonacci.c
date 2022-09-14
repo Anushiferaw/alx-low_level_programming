d027cd17e0d23328cf8bffbba240a8aacd2cd000
anushiferaw <anuhabyba@gmail.com>
2022-09-14 15:58:39 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,7 +20,8 @@ hold1 = (bk1 + bk2) / LARGEST;
 hold2 = (bk1 + bk2) % LARGEST;
 hold3 = fr1 + fr2 + hold1;
 fr1 = fr2, fr2 = hold3;
-bk1 = bk2, bk2 = hold2;	printf("%lu%010lu", fr2, bk2);
+bk1 = bk2, bk2 = hold2;	
+printf("%lu%010lu", fr2, bk2);
 }
 else
 {
@@ -28,8 +29,9 @@ hold2 = bk1 + bk2;
 bk1 = bk2, bk2 = hold2;	
 printf("%lu", bk2);
 }
-if (count != 97)printf(", ");
+if (count != 97)
+printf(", ");
+}
 printf("\n");
 return (0);
-}
 }
