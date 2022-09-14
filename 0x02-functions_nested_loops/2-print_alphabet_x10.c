9b6ddcc88dc9523640e5174c935f4411150a25f5
anushiferaw <anuhabyba@gmail.com>
2022-09-14 02:20:03 -0700

    print alphabet 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_10 - function to print abc 10 times, followed by a new line
+ * Return: Always 0 (Success) 
+ */
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i=0; i<= 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c)	
+}
+_putchar('\n')
+}
+}
