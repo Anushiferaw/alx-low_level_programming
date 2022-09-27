26920a41cdc9661ebb42e9065b93528cf0446f74
anushiferaw <anuhabyba@gmail.com>
2022-09-26 17:31:51 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,7 +10,8 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i, j;
-for (i = 0; haystack[1] != '\0'; i++)
+for (i = 0; haystack[i] != '\0'; i++)
+{
 for (j = 0; needle[j] != '\0'; j++)
 {
 if (haystack[i + j] != needle[j])
